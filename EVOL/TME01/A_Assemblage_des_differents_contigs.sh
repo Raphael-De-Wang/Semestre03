39c1b8742125a9a0826afc2ab6074c8aacac50bb
WANG Jinxin <wangjinxin.fr@gmail.com>
2015-10-08 20:06:16 +0200

    EVOL TME 01

--- EVOL/TME01/A_Assemblage_des_differents_contigs.sh
+++ EVOL/TME01/A_Assemblage_des_differents_contigs.sh
@@ -15,9 +15,9 @@ export PATH=/usr/local/ncbi-blast-2.2.29+/bin/:$PATH
 # python assemblage_contigs_blastn_table.py contigs.blastn
 
 # 
-python extract_sequence_start_end.py contigs.fasta 50 contigs_head_tail.fasta
+python extract_sequence_start_end.py contigs.fasta 40 contigs_head_tail.fasta
 makeblastdb -in contigs_head_tail.fasta -input_type 'fasta' -out 'contigsHeadTailDB' -dbtype 'nucl'
-blastn -db 'contigsHeadTailDB' -query contigs_head_tail.fasta -strand 'both' -out contigs_head_tail.blastn -outfmt '7'
+blastn -db 'contigsHeadTailDB' -query contigs_head_tail.fasta -strand 'both' -out contigs_head_tail.blastn -outfmt '6'
 
 # combine contigs
 # python combine_contigs.py -in contigs.fasta -on combine_contigs.fasta -o 8 3 2 7 4 5 -s 0 0 0 0 1 0 -L 143 98 127 100 158
---
+++ EVOL/TME01/brutal_cmp.py
@@ -0,0 +1,11 @@
+#!env python
+
+import argparse
+
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.Alphabet import generic_protein
+
+def interface_standard():
+    
