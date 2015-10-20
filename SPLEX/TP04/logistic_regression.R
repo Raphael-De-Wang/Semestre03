7f84f695959e238152ce2571a0ae61a169467274
WANG Jinxin <wangjinxin.fr@gmail.com>
2015-10-20 23:01:42 +0200

    SPLEX TME 04 debug undone

--- SPLEX/TP04/logistic_regression.R
+++ SPLEX/TP04/logistic_regression.R
@@ -17,12 +17,25 @@ fakeData <- function (n=1000) {
 
 fakeData2 <- function () {
     x0 = rep(1,4)
-    x1 = c(1,1,-1,-1)
-    x2 = c(1,-1,1,-1)
+    x1 = c(1,2,-3,-4)
+    x2 = c(1,-2,3,-4)
     y  = c(0,0,1,1)
     df = data.frame(y=y,x0=x0,x1=x1,x2=x2)
     return(list(x0=x0,x1=x1,x2=x2,y=y,frame=df,numEchant=4,nomFeauture=2))
 }
+
+fakeData3 <- function(n1,n2) {
+    randOrder <- sample(1:(n1+n2))
+    x0 = rep(1,n1+n2)
+    x1 <- c(rnorm(n1, mean = -5),rnorm(n2, mean =  10))[randOrder]
+    x2 <- c(rnorm(n1, mean = -5),rnorm(n2, mean =  10))[randOrder]
+    y <- c(rep(0,n1),rep(1,n2))[randOrder]
+    df = data.frame(y=y,x0=x0,x1=x1,x2=x2)
+    numEchant <- n1 + n2
+    numFeature <- 2
+    return(list(x0=x0,x1=x1,x2=x2,y=y,frame=df,"numEchant"=numEchant,"numFeature"=numFeature))
+}
+
 
 Q1.SimulezJeu <- function () {
     pdf(file=ifelse(FALSE, "tp04_Q1SimulezJeu.pdf", "tp04_Q1SimulezJeu.pdf"))
@@ -40,24 +53,20 @@ Q1.SimulezJeu <- function () {
     return(glm.binomial)
 }
 
-#### La regression logistique binaire
+#### La regression logistique 
 
 randInitTheta <- function (numFeature=2) {
-    # return(as.matrix(rnorm(numFeature+1),row=1))
-    # return(matrix(rep(1,numFeature+1),ncol=1))
-    return(matrix(c(0,1,1),ncol=1))
+    return(matrix(c(1,-1,1),ncol=1))
+}
+
+logVrai <- function (d, theta) {
+    x <- as.matrix(d$frame)[,-1]
+    y <- as.matrix(d$y)
+    return(sum(unlist(lapply(1:nrow(x),function(n,theta,x,y){log(1+exp(t(theta)%*%x[n,]))-y[n]*t(theta)%*%x[n,]},theta,x,y))))
 }
 
 vectorXmatrix <- function (theta,X) {
     return(unlist(lapply(c(1:nrow(X)),function(n,theta,X){t(theta)%*%X[n,]},theta,X)))
-}
-
-logVrai <- function (d, theta) {
-    # remove y
-    x <- as.matrix(d$frame)[,-1]
-    y <- as.matrix(d$y)
-    thetaX <- vectorXmatrix(theta,x)
-    return(sum(log(exp(thetaX)+1)-y*thetaX))
 }
 
 p <- function (theta, x, y = 1) {
@@ -70,13 +79,12 @@ p <- function (theta, x, y = 1) {
 }
 
 firstDerivative <- function (theta,x,y) {
-    return(vectorXmatrix((y-p(theta,x)),t(x)))
+    a <- lapply(1:nrow(x),function(n,prob,x,y){return(x[n,]*(prob[n]-y[n]))},p(theta,x),x,y)
+    return(colSums(matrix(unlist(a),nrow = nrow(x), byrow = TRUE)))
 }
 
 computeHessianMtx <- function (theta,x) {
-    mxv <- function(n,x,prob) {
-        return(tcrossprod(x[n,])*prob[n]*(1-prob[n]))
-    }
+    mxv <- function(n,x,prob) { return(tcrossprod(x[n,])*prob[n]*(1-prob[n])) }
     H <- matrix(rep(0,ncol(x)^2),nrow=ncol(x))
     for ( hssn in lapply(1:nrow(x),mxv,x,p(theta,x))) {
         H <- H + hssn
@@ -88,7 +96,7 @@ dvp <- function (d,theta) {
     theta <- theta/theta[2]
     plot(d$x1,d$x2,type="n")
     text(d$x1,d$x2,labels = d$y,col = c('red','blue','green')[d$y+1])
-    abline(a = sum(theta * c(1,0,0)), b = -theta[3]/theta[2] , col = 2)
+    abline(a = -theta[1]/theta[3], b = -theta[2]/theta[3] , col = 2)
 }
 
 basefuns.testcase <- function(n){
@@ -109,18 +117,25 @@ basefuns.testcase <- function(n){
 
     x <- matrix(unlist(fdata$frame[,-1]), nrow=fdata$numEchant)
     y <- matrix(unlist(fdata$y))
+
     # probability
     pRes <- p(theta,x)
     print(fdata$y)
     print(sprintf("y=1 probability : %f", pRes))
 
-    # Compute the first derivative
+    print("Compute the first derivative : ")
     print(firstDerivative(theta,x,y))
 
-    # Compute the Hessian matrix
+    print("Compute the Hessian matrix : ")
     print(computeHessianMtx(theta,x))
 
 }
+
+
+matrixXvector <- function(m,v) {
+    return(matrix(unlist(lapply(1:nrow(m),function(n,m,v){m[n,]%*%v},m,v))))
+}
+
 
 NewtonRaphon <- function (d,eps=1.e-5,maxIter=500) {
     lvlist <- c()
@@ -128,15 +143,14 @@ NewtonRaphon <- function (d,eps=1.e-5,maxIter=500) {
     x <- matrix(unlist(d$frame[,-1]), nrow=d$numEchant)
     y <- matrix(unlist(d$y))
     # iterate to the convergence
-    while ( (length(lvlist)<2 || abs(lvlist[1]-lvlist[2])>eps) && (length(lvlist) < maxIter) ) {
+    while ( (length(lvlist) < 2 || abs(lvlist[1]-lvlist[2])>eps) && (length(lvlist) < maxIter) ) {
         dvp(d,theta)
         lvlist <- c(logVrai(d,theta),lvlist)
         print(sprintf("iteration number [%d] [%f]", length(lvlist), lvlist[1]))
         print(theta)
         sh <- solve(computeHessianMtx(theta,x))
         fd <- firstDerivative(theta,x,y)
-        theta <- theta - vectorXmatrix(fd,sh)
-        theta <- theta/theta[2]
+        theta <- theta - matrixXvector(sh,fd)
     }
     return(theta)
 }
@@ -144,13 +158,12 @@ NewtonRaphon <- function (d,eps=1.e-5,maxIter=500) {
 NewtonRaphon.testcase <- function(n) {
     pdf(file=ifelse(FALSE, "Newtown_test.pdf", "Newtown_test.pdf"))
     attach(mtcars,warn.conflicts = FALSE)
-
-    d <- fakeData(n)
-    # d <- fakeData2()
-    theta <- NewtonRaphon(d,maxIter=6)
+    # d <- fakeData(n)
+    d <- fakeData2()
+    # d <- fakeData3(10,20)
+    theta <- NewtonRaphon(d)
     dvp(d,theta)
-
-    dev.off()    
+    dev.off()
 }
 
 # glm.binomial <- Q1.SimulezJeu()
