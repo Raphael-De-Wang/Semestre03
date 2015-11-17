c39e3016bd11fbbd9154f51913025e864bc5a80d
Raphael-De-Wang <wangjinxin.fr@gmail.com>
2015-11-17 06:47:50 +0100

    Resume

--- PHYG/TME03/TME3/recombination_simulator/recombination_simulator/sim_yeast_step3_analyze.R
+++
@@ -1,9 +0,0 @@
-# Author: Raphael Champeimont
-# UMR 7238 Biologie Computationnelle et Quantitative
-
-# setwd("~/Documents/PhyChro/sim")
-
-params <- list(
-  outDirectory = "results_yeast1/")
-
-source("scripts/sim_analyze_phychro.R")
